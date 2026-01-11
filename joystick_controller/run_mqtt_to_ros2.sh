a0c68f1417a19e91b8f3c539a6d768129a39b54b
Tuan V. Ngo <tuannv.email@gmail.com>
2026-01-11 19:35:46 +0800

    Abto to control robot remotely

--- joystick_controller/run_mqtt_to_ros2.sh
+++ joystick_controller/run_mqtt_to_ros2.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+readonly RUN_MQTT_TO_ROS2_SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+echo "RUN_MQTT_TO_ROS2_SCRIPT_DIR: $RUN_MQTT_TO_ROS2_SCRIPT_DIR"
 
-source $SCRIPT_DIR/setup.sh
-source $SCRIPT_DIR/run_mqtt_to_ros2.sh.conf
+source $RUN_MQTT_TO_ROS2_SCRIPT_DIR/../setup.sh
+source $RUN_MQTT_TO_ROS2_SCRIPT_DIR/run_mqtt_to_ros2.sh.conf
 
 ros2 run joystick_controller mqtt_to_ros2_bridge.py --ros-args -p mqtt_broker:=$MQTT_BROKER -p mqtt_port:=$MQTT_PORT
--- setup.sh
+++ setup.sh
@@ -7,6 +7,4 @@ echo "Setup unitree ros2 environment"
 source $SCRIPT_DIR/install/setup.zsh
 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                        <NetworkInterface name="eth0" priority="default" multicast="default" />
-                        </Interfaces></General></Domain></CycloneDDS>'
+export CYCLONEDDS_URI="file://${SCRIPT_DIR}/cyclonedds.xml"
