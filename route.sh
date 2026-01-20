773290fdadca150d8561d122207bae2f8a0eeff1
Tuan V. Ngo <tuannv.email@gmail.com>
2026-01-20 17:06:04 +0800

    Update server IP

--- route.sh
+++ route.sh
@@ -4,7 +4,7 @@
 sudo ip route add 10.1.100.250/32 via 192.168.1.1
 
 # Xinmatrix
-sudo ip route add 10.1.101.210/32 via 192.168.1.1
+sudo ip route add 10.1.106.210/32 via 192.168.1.1
 
 # GH81/GH82
 sudo ip route add 10.1.101.211/32 via 192.168.1.1
