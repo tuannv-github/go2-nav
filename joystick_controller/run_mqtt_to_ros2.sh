648aa7d293513feed8f8017733e171ee3d4a2c2e
Tuan V. Ngo <tuannv.email@gmail.com>
2026-02-07 23:19:57 +0800

    feat: Configure CycloneDDS as the RMW implementation and update the setup script sourcing.

--- joystick_controller/run_mqtt_to_ros2.sh
+++ joystick_controller/run_mqtt_to_ros2.sh
@@ -3,7 +3,12 @@
 readonly RUN_MQTT_TO_ROS2_SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "RUN_MQTT_TO_ROS2_SCRIPT_DIR: $RUN_MQTT_TO_ROS2_SCRIPT_DIR"
 
-source $RUN_MQTT_TO_ROS2_SCRIPT_DIR/../setup.sh
+PROJECT_DIR=$RUN_MQTT_TO_ROS2_SCRIPT_DIR/../
+
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+export CYCLONEDDS_URI="file://${PROJECT_DIR}/cyclonedds.go2.xml"
+
+source $PROJECT_DIR/install/setup.zsh
 source $RUN_MQTT_TO_ROS2_SCRIPT_DIR/run_mqtt_to_ros2.sh.conf
 
 ros2 run joystick_controller mqtt_to_ros2_bridge.py --ros-args -p mqtt_broker:=$MQTT_BROKER -p mqtt_port:=$MQTT_PORT
