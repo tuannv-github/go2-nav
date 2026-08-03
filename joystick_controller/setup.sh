207c5a2d76be2ba8a059f6da89e4967cee13a7f3
Tuan V. Ngo <tuannv.email@gmail.com>
2026-08-03 18:22:09 +0800

    chore: update GitHub templates and CI workflows while standardizing joystick setup to use python3-dev

--- joystick_controller/setup.sh
+++ joystick_controller/setup.sh
@@ -5,8 +5,8 @@ set -e
 
 echo "Setting up joystick_controller..."
 
-# python3.11-dev provides Python.h required to build evdev from source
-sudo apt install -y python3.11-venv python3.11-dev build-essential
+# python3-dev provides Python.h required to build evdev from source
+sudo apt install -y python3-venv python3-dev build-essential
 
 # Create virtual environment if it doesn't exist
 if [ ! -d "venv" ]; then
